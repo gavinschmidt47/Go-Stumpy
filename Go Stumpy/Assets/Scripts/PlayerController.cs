a325192c525c4b9f24ff3855720e6aedef0740f8
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-01-28 15:02:33 -0500

    Setting Up Ability NOT DONE

--- Go Stumpy/Assets/Scripts/PlayerController.cs
+++ Go Stumpy/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     //Public Variables
-    public float speed = 5f;
-    public float jumpHeight = 5f;
+    public float baseSpeed = 5f;
+    public float baseJumpHeight = 5f;
+    public float maxSuckTime = 10f;
     public InputAction movement;
     public InputAction jump;
     public InputAction useAbility;
-    
     public AbilitiesScript abilities;
 
     //Private Variables
     private Rigidbody2D rb;
     private Vector2 moveDirection;
-    
+    private bool abilityOn = false;
+    private bool usingAbility = false;
+    private float speed;
+    private float jumpHeight;
+
+    //Enable Input Actions
     private void OnEnable()
     {
         movement.Enable();
@@ -25,6 +30,7 @@ public class PlayerController : MonoBehaviour
         useAbility.Enable();
     }
 
+    //Disable Input Actions
     private void OnDisable()
     {
         movement.Disable();
@@ -32,39 +38,75 @@ public class PlayerController : MonoBehaviour
         useAbility.Disable();
     }
 
+    //Start and Update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        //Disable Cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //set speed and jump height
+        speed = baseSpeed;
+        jumpHeight = baseJumpHeight;
     }
 
     void Update()
     {
+        //Read Input
         moveDirection = movement.ReadValue<Vector2>();
         if (jump.triggered && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
-            Debug.Log("Jump");
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        
+        if (useAbility.IsPressed())
+        {
+            StartCoroutine(UseAbility());
+            usingAbility = true;
+        }
+        else
+        {
+            usingAbility = false;
         }
     }
 
     void FixedUpdate()
     {
+        //Move Player
         rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
     }
     
     //Kirby Ability
-    private void UseAbility()
+    private IEnumerator UseAbility()
     {
+        Debug.Log("Ability Used");
+        //Detect Enemy
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 10f);
+
+        //Deactivate Previous Ability
+        if (abilityOn)
+        {
+            abilityOn = false;
+            speed = baseSpeed;
+            jumpHeight = baseJumpHeight;
+        }
+
+        //Activate Ability
         if (hit.collider != null && hit.collider.CompareTag("Jumpy"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, abilities.jumpBoost);
+            jumpHeight += abilities.jumpBoost;
             abilities.jumpParticles.Play();
+            abilityOn = true;
         }
         else if (hit.collider != null && hit.collider.CompareTag("Speedy"))
         {
             speed += abilities.speedBoost;
             abilities.speedParticles.Play();
+            abilityOn = true;
         }
+
+        yield return new WaitForSeconds(maxSuckTime);
     }
 }
