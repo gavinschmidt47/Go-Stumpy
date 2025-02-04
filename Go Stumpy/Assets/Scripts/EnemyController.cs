81940ab353e244f3500160823b213365fc7e42ef
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-02-04 11:15:58 -0500

    Added Camera follow

--- Go Stumpy/Assets/Scripts/EnemyController.cs
+++ Go Stumpy/Assets/Scripts/EnemyController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    //Public Variables
-    public GameInfo gameInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
